1a11e5ef33bdd55a0b2026dd61ea3cc0ab5b0c08
Harriet Melesi <harrietmelesi@Harriets-iMac.home>
2020-07-09 18:43:25 +0100

    added images

--- app/config/characters.js
+++ app/config/characters.js
@@ -4,39 +4,74 @@ export default [
     name: "Rick",
     line:
       'I hate to break it to you but what people call "love" is just a chemical reaction that compels animals to breed.',
+    image: require("../assets/rick.png"),
   },
   {
     id: 2,
     name: "Morty",
     line:
       "Gaslighting doesn’t exist, you made it up because you’re f*****g crazy.",
+    image: require("../assets/morty.png"),
   },
-  { id: 3, name: "Jerry", line: "Why Am I So Mediocre?" },
-  { id: 4, name: "Beth", line: "I'm sorry you feel you deserve an apology." },
+  {
+    id: 3,
+    name: "Jerry",
+    line: "Why Am I So Mediocre?",
+    image: require("../assets/jerry.png"),
+  },
+  {
+    id: 4,
+    name: "Beth",
+    line: "I'm sorry you feel you deserve an apology.",
+    image: require("../assets/beth.png"),
+  },
   {
     id: 5,
     name: "Summer",
     line:
       "Losers look up stuff while the rest of us are carpe'n all them diems.",
+    image: require("../assets/summer.png"),
   },
   {
     id: 6,
     name: "Birdperson",
     line: "It has been a challenging mating season for Birdperson.",
+    image: require("../assets/birdp.png"),
   },
-  { id: 7, name: "Squanchy", line: "I like your squanch." },
-  { id: 8, name: "Mr. Poopybutthole", line: "Ooo-wee!" },
-  { id: 9, name: "Tammy", line: "You know what this human eats." },
+  {
+    id: 8,
+    name: "Mr. Poopybutthole",
+    line: "Ooo-wee!",
+    image: require("../assets/mrpoo.png"),
+  },
+  {
+    id: 9,
+    name: "Tammy",
+    line: "You know what this human eats.",
+    image: require("../assets/tammy.png"),
+  },
   {
     id: 10,
     name: "Abrodolf Lincler",
     line: "Prepare to be emancipated from your own inferior genes.",
+    image: require("../assets/abro.png"),
   },
   {
     id: 11,
     name: "Fart",
     line: "You are not like other carbon-based life forms.",
+    image: require("../assets/fart.png"),
   },
-  { id: 12, name: "Butter Robot", line: "What is my purpose?" },
-  { id: 13, name: "Crocubot", line: "I am Crocubot." },
+  {
+    id: 12,
+    name: "Butter Robot",
+    line: "What is my purpose?",
+    image: require("../assets/butter.png"),
+  },
+  {
+    id: 13,
+    name: "Crocubot",
+    line: "I am Crocubot.",
+    image: require("../assets/croc.png"),
+  },
 ];
